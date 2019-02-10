5b8d466d21513b95574ed27748234f42e41f13ca
Víctor Navarro González <victor.navarro@enti.cat>
2019-02-10 12:54:05 +0100

    Basic form autocomplete.

--- static/scripts/searchbar.js
+++ static/scripts/searchbar.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
     resetSearchbar();
+    autocomplete(2);
 });
 
 function resetSearchbar() {
-    // TODO: Research a more efficient way of doing this.
     let searchBar = $("#search-bar-error");
     let searchInput = $(".search-input-error");
 
@@ -23,15 +23,100 @@ function resetSearchbar() {
         searchBar = $("#search-bar");
     }
 
-    console.log(searchInput);
-
+    // Reset the search bar when clicked.
     searchInput.click(function () {
         searchBar[0].id = "search-bar";
         searchInput[0].className = "search-input";
         searchInput[0].placeholder = "";
     });
 
+    // Rewrite placeholder when the input is deselected.
     searchInput.on('blur', function () {
         searchInput[0].placeholder = "Search you favorite game...";
     });
+}
+
+function autocomplete(minimumCharacters) {
+
+    let searchInput = $(".search-input");
+    let formContainer = $("#form-container");
+    let linksContainer = $("#links");
+
+    let names = [];
+
+    // Populate the names array with the contents of the JSON.
+    $.getJSON("static/data/game_names.json", function (data) {
+        $.each(data, function (key, value) {
+            names.push(value.toString());
+        })
+    });
+
+    searchInput.on("change keyup paste focus", function () {
+        // Clear list each time the user changes the input.
+        $(".prediction").remove();
+
+        if (searchInput.val().length >= minimumCharacters) {
+            let matches = getMatches(searchInput.val(), names, 5);
+
+            // Hide and show the links container if there are matches.
+            if (matches.length > 0) {
+                linksContainer.hide();
+            } else {
+                linksContainer.show();
+            }
+
+            $.each(matches, function (index, value) {
+                // Create a new prediction element.
+                // TODO: Make the new element using JQuery.
+                let prediction = document.createElement("DIV");
+                prediction.setAttribute("class", "prediction");
+                prediction.innerText = value;
+
+                // Add a click listener to the new element.
+                $(prediction).click(function () {
+                    // Update the input text.
+                    let innerText = $(this)[0].innerText;
+                    searchInput.val(innerText);
+
+                    // Update the prediction list again.
+                    searchInput.focus();
+                });
+
+                formContainer.append(prediction);
+            });
+        }
+
+        if (searchInput.val() === "") {
+            linksContainer.show();
+        }
+    });
+}
+
+
+function getMatches(inputName, names, number) {
+    // For performance reasons, lower case the input one time.
+    let nameToMatch = inputName.toLowerCase();
+
+    let matches = [];
+    let counter = 0;
+
+    // Iterate over names until number five of elements contain the name to match.
+    $.each(names, function (index, value) {
+        if (value.toLowerCase().includes(nameToMatch)) {
+            matches.push(value);
+            counter++;
+        }
+
+        // Exit the foreach loop.
+        if (counter === number) {
+            return false;
+        }
+    });
+
+    // Check if there is only one match and is exactly the one inputted.
+    if (matches.length === 1 && matches[0].toLowerCase() === nameToMatch) {
+        return [];
+    }
+
+    return matches;
 }
