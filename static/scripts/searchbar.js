8a177630328c9e042e2fb96ccfcd1b4c466c0695
Víctor Navarro González <victor.navarro@enti.cat>
2019-02-13 19:07:11 +0100

    Optimized auto-prediction.

--- static/scripts/searchbar.js
+++ static/scripts/searchbar.js
@@ -4,47 +4,33 @@ $(document).ready(function () {
 });
 
 function resetSearchbar() {
-    let searchBar = $("#search-bar-error");
-    let searchInput = $(".search-input-error");
-
-    if (searchInput.length === 0) {
-        searchInput = $(".search-input-warning");
-    }
-
-    if (searchBar.length === 0) {
-        searchBar = $("#search-bar-warning");
-    }
-
-    if (searchInput.length === 0) {
-        searchInput = $(".search-input");
-    }
-
-    if (searchBar.length === 0) {
-        searchBar = $("#search-bar");
-    }
+    let searchBar = $(".search-bar");
+    let searchInput = $(".search-input");
 
     // Reset the search bar when clicked.
     searchInput.click(function () {
-        searchBar[0].id = "search-bar";
-        searchInput[0].className = "search-input";
-        searchInput[0].placeholder = "";
+        searchBar.removeAttr("id");
+
+        $(this).removeClass("search-input-error");
+        $(this).removeClass("search-input-warning");
+        $(this).attr("placeholder", "");
     });
 
     // Rewrite placeholder when the input is deselected.
     searchInput.on('blur', function () {
-        searchInput[0].placeholder = "Search you favorite game...";
+        $(this).attr("placeholder", "Search you favorite game...");
     });
 }
 
 function autocomplete(minimumCharacters) {
-
+    // Find required elements.
     let searchInput = $(".search-input");
+    let searchBar = $(".search-bar");
     let formContainer = $("#form-container");
     let linksContainer = $("#links");
 
-    let names = [];
-
     // Populate the names array with the contents of the JSON.
+    let names = [];
     $.getJSON("static/data/game_names.json", function (data) {
         $.each(data, function (key, value) {
             names.push(value.toString());
@@ -59,32 +45,24 @@ function autocomplete(minimumCharacters) {
             let matches = getMatches(searchInput.val(), names, 5);
 
             // Hide and show the links container if there are matches.
-            if (matches.length > 0) {
-                linksContainer.css('visibility', 'hidden')
-            } else {
-                linksContainer.css('visibility', 'visible')
-            }
+            linksContainer.css('visibility', matches.length > 0 ? 'hidden' : 'visible');
 
             $.each(matches, function (index, value) {
                 // Create a new prediction element.
-                // TODO: Make the new element using JQuery.
-                let prediction = document.createElement("DIV");
-                prediction.setAttribute("class", "prediction");
-                prediction.innerText = value;
+                let prediction = $("<div></div>").attr("class", "prediction").text(value);
 
                 // Add a click listener to the new element.
                 $(prediction).click(function () {
-                    // Update the input text.
-                    searchInput.val($(this)[0].innerText);
-
-                    // Update the prediction list again.
-                    searchInput.focus();
+                    // Update the input text and focus.
+                    searchInput.val($(this).text());
+                    searchBar.submit();
                 });
 
                 formContainer.append(prediction);
             });
         }
 
+        // If the input field is empty, make the links visible again.
         if (searchInput.val() === "") {
             linksContainer.css('visibility', 'visible')
         }
@@ -92,30 +70,12 @@ function autocomplete(minimumCharacters) {
 }
 
 
-function getMatches(inputName, names, number) {
+function getMatches(inputName, names, max) {
     // For performance reasons, lower case the input one time.
     let nameToMatch = inputName.toLowerCase();
 
-    let matches = [];
-    let counter = 0;
-
     // Iterate over names until number five of elements contain the name to match.
-    $.each(names, function (index, value) {
-        if (value.toLowerCase().includes(nameToMatch)) {
-            matches.push(value);
-            counter++;
-        }
+    let matches = names.filter(name => name.toLowerCase().includes(nameToMatch));
 
-        // Exit the foreach loop.
-        if (counter === number) {
-            return false;
-        }
-    });
-
-    // Check if there is only one match and is exactly the one inputted.
-    if (matches.length === 1 && matches[0].toLowerCase() === nameToMatch) {
-        return [];
-    }
-
-    return matches;
+    return matches.slice(0, max);
 }
